ea2227e9551e0e939b914079e45353618566c264
jysong0101 <juyeop0101@khu.ac.kr>
2024-10-23 18:00:33 +0900

    clean log update

--- clean-logs.py
+++ clean-logs.py
@@ -4,12 +4,10 @@ from datetime import datetime
 
 LOGS_DIRECTORY = "logs"
 
-deleted_files_count = 0
+# deleted_files_count = 0
 
-# 디렉토리 내 파일 확인
 for filename in os.listdir(LOGS_DIRECTORY):
     file_path = os.path.join(LOGS_DIRECTORY, filename)
 
-    # 파일 삭제
     os.remove(file_path)
-    deleted_files_count += 1
+    # deleted_files_count += 1
